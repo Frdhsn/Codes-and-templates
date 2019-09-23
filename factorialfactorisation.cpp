c482b10f01ad3b1dadd52a1a50189cdc21bdf211
Frdhsn <iamforu299@gmail.com>
2019-09-23 08:59:07 +0600

    Create factorialfactorisation.cpp

---
+++ factorialfactorisation.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+typedef pair<int,int> pii;
+const int MAX = 1e6 + 10;
+int id,primes[78500];
+bool isprime[MAX];
+void sieve()
+{
+    primes[0]=2;
+    for(int i=4;i<MAX;i+=2)isprime[i]=1;
+    for(int i=3;i<MAX;i+=2){
+        if(isprime[i]==0){
+            primes[++id]=i;
+            if(i+i<MAX)
+            for(int j=i+i;j<MAX;j+=i)
+                isprime[j]=1;
+        }
+    }
+}
+vector<pii> factfactorise(int n)
+{
+    vector<pii> F;
+    for(int i=0;i<id&&primes[i]<=n;i++){
+        int curr = primes[i];
+        //if(n%curr==0){
+            int num = n ;
+            int cnt = 0 ;
+            while( num / curr){
+                cnt += num/curr;
+                curr*=primes[i];
+            }
+            if(cnt)
+            F.push_back({primes[i] , cnt});
+        //}
+    }
+    return F;
+}
+int main ()
+{
+    sieve();
+    int n;
+    while(cin >> n && n){
+            vector<pii> F = factfactorise(n);
+            for(int i=0;i<F.size();i++){
+                cout<<F[i].first<<" "<<F[i].second<<endl;
+            }
+    }
+    return 0;
+}
