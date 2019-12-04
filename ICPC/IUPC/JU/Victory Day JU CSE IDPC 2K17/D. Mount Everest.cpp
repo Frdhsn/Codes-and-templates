b8baa595be22c1fdb9f09db606ec5aa52cb91aa8
Frdhsn <iamforu299@gmail.com>
2019-12-05 01:02:24 +0600

    Create D. Mount Everest.cpp

---
+++ ICPC/IUPC/JU/Victory Day JU CSE IDPC 2K17/D. Mount Everest.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cout<<8848-n<<endl;
+    }
+    return 0;
+}
