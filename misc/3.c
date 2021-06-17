44e754c4975871fabaf2076d98cd4282e7c51964
Frdhsn <iamforu299@gmail.com>
2021-06-17 22:45:32 +0600

    Create 3.c

---
+++ misc/3.c
@@ -0,0 +1,12 @@
+// F = ma
+#include<stdio.h>
+int main () {
+	float a, m, F;
+	printf("Enter m = ");
+	scanf("%f", &m);
+	printf("Enter a = ");
+	scanf("%f", &a);
+	F = m * a;
+	printf("F = %f\n", F);
+	return 0;
+}
