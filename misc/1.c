20b10c90bcdc428b0303958b49409c8f08c990ff
Frdhsn <iamforu299@gmail.com>
2021-06-17 22:41:41 +0600

    Create 1.c

---
+++ misc/1.c
@@ -0,0 +1,12 @@
+// s = d/t
+#include<stdio.h>
+int main () {
+	float s, d, t;
+	printf("Enter d = ");
+	scanf("%f", &d);
+	printf("Enter t = ");
+	scanf("%f", &t);
+	s = d / t;
+	printf("s = %f\n", s);
+	return 0;
+}
